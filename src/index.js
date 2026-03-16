2b033c886340471261c4b5563ee3e7cce82dcc44
IlyaDuzhakov <duzhakovip@mail.ru>
2026-03-16 09:10:28 +0400

    change icons

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/railway_service">
     <React.StrictMode>
       <App />
     </React.StrictMode>
